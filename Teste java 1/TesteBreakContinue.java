6d5cdc02ec7950f642107d3984c96313359e6643
Djeymison <Djeymisonwork@gmail.com>
2022-09-23 11:24:53 -0300

    MinhasNotasDoPC

---
+++ Teste java 1/TesteBreakContinue.java
@@ -0,0 +1,16 @@
+
+
+public class TesteBreakContinue {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        for (int numero = 1; numero <=10; numero ++){
+            if  (numero == 6)
+            System.out.println(numero + "ALERTA!!!..");
+                continue;
+      //      System.out.println(numero);
+        }
+    }
+    
+}
---
+++ projeto1.java
@@ -0,0 +1,19 @@
+public class projeto1 {
+   // public static void main(String[] args) {
+ //       System.out.println("Primeiro teste Java fííííííí...... To fudido XD");
+// public class app(){
+
+    public static void main(String[] args) {
+
+        int[] numeros = {2,3,5,7,11,13,18,34};
+
+        for( int i = 0;i <= numeros.length -1; i++){
+            if(numeros[i]%2==0){
+                System.out.println(numeros[i]);
+            }
+        } 
+    }
+}
+    
+
+    
